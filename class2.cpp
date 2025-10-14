66424017c5bdde0fa1fb300864248f810afbd928
Ajitesh <ajiteshsrivastava27@gmail.com>
2025-10-14 13:24:14 +0530

    Ankit

---
+++ class2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+#include <climits>
+// #include<bits/stdc++.h>
+int main(){
+    //int x = INT_MAX +1 ; 
+    //cout<<x<<endl;
+
+    string s;
+    // cin >> s;
+    // cout << s << endl;
+    getline(cin,s);
+
+}
---
+++ class3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <bits\stdc++.h>
+using namespace std;
+int main()
+{
+    // int x =3;
+    // int y = 'a';
+    // if(x<y) cout << "y is greater" << endl;
+    // else cout << "x is greater";
+
+    // float x =5.55;
+    // int y = int(x);
+    // cout << y << endl;
+
+    int x;
+    cin >> x;
+    if (x % 9 == 0)
+        cout << "Divisible by 9" << endl;
+    else
+        cout << "Not divisible by 9" << endl;
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+
+
+}
